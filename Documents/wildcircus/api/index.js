fc6c395f0a13e0b1bdbe1a6d69cc27d64e73d8aa
Alexandre Coutant <alexcoutant3@gmail.com>
2019-07-16 17:41:10 +0200

    call api show, design

--- Documents/wildcircus/api/index.js
+++ Documents/wildcircus/api/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const mysql = require('mysql')
 
 const api = express();
+
+api.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
+  database: "wildcircus",
   password: "Wildcodelearning000,"
 });
 
@@ -16,9 +23,40 @@ connection.connect((err) => {
 
 api.get('/', (req, res) => {
   res.send('ok?good')
-})
+});
+
+api.get('/show', (req, res) => {
+  connection.query('SELECT * FROM shows', (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  })
+});
+
+api.get('/show/filter?', (req, res) => {
+  if(req.query.price){
+    if(req.query.price === "asc") {
+      connection.query('SELECT * FROM shows ORDER BY price ASC', (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      });
+    } else if(req.query.price === "desc") {
+      connection.query('SELECT * FROM shows ORDER BY price DESC', (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      });
+    } else {
+      res.sendStatus(400);
+    }
+  } else {
+    res.sendStatus(400);
+  }
+  // connection.query('SELECT * FROM shows', (err, result) => {
+  //   if (err) throw err;
+  //   res.send(result);
+  // })
+});
 
 api.listen(8000,(err) => {
   if(err) throw err;
     console.log('Api ok..');
-})
+});
--- Documents/wildcircus/src/Components/Show/Show.js
+++ Documents/wildcircus/src/Components/Show/Show.js
@@ -1,86 +1,96 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import _ from 'underscore';
-import { connect } from 'react-redux';
 
 import Cart from '../Cart/Cart';
-import { addArticle } from '../../Actions/cartActions';
 
-import './ListArticles.css';
+import './Show.css';
 
-function ListArticles(props) {
+function Show(props) {
 
-    const [articles, setArticles] = useState([]);
-    const [nameFilter, setNameFilter] = useState(false);
-    const [priceFilter, setPriceFilter] = useState(false);
+  const [shows, setShows] = useState([]);
+  const [filterPrice, setfilterPrice] = useState(false);
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/articles')
-            .then((result) => {
-                setArticles(result.data);
-            })
-    }, [])
-
-    const filterArticle = (tag, [setFunc, param]) => {
-        if (param) {
-            setArticles(articles.reverse());
-        } else {
-            setArticles(_.sortBy(articles, tag));
-        }
-        setFunc(!param);
+  const toggleFilterPrice = () => {
+    if (filterPrice === 0) {
+      setfilterPrice(1);
+    } else if (filterPrice === 1) {
+      setfilterPrice(2);
+    } else {
+      setfilterPrice(1);
     }
+  }
 
-    return (
-        <div className="row mt-5">
-            <div className="col-md-8">
-                <div className="card">
-                    <div className="card-body">
-                        <div className="card-title">
-                            <p className="lead">Articles</p>
-                        </div>
-                        <table className="table-striped">
-                            <thead>
-                                <tr>
-                                    <th className="filter-button" onClick={() => filterArticle('name', [setNameFilter, nameFilter])}>Name</th>
-                                    <th className="filter-button" onClick={() => filterArticle('price', [setPriceFilter, priceFilter])}>Price</th>
-                                    <th>Quantity</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {articles.map((article, index) => {
-                                    return (
-                                        <tr className="py-2 tr-height" key={index}>
-                                            <th className="px-2" scope="row" width="50%">{article.name}</th>
-                                            <td width="20%">{article.price} $</td>
-                                            <td width="10%">{article.quantity}</td>
-                                            <td width="20%">
-                                                <button onClick={() => props.dispatch(addArticle(article))} className="btn btn-primary btn-sm float-right mr-2">Add to cart</button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="card-footer">
-                        <div className="pagination">
-                            <li className="page-item">
-                                <a className="page-link" href="/#">1</a>
-                            </li>
-                            <li className="page-item">
-                                <a className="page-link" href="/#">2</a>
-                            </li>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Cart />
+  useEffect(() => {
+    if (filterPrice === 1) {
+      axios.get('http://localhost:8000/show/filter?price=asc')
+        .then((result) => {
+          setShows(result.data);
+        })
+    } else if (filterPrice === 2) {
+      axios.get('http://localhost:8000/show/filter?price=desc')
+        .then((result) => {
+          setShows(result.data);
+        })
+    }
+  }, [filterPrice])
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/show')
+      .then((result) => {
+        setShows(result.data);
+      })
+  }, [])
+
+  // const filterArticle = (tag, [setFunc, param]) => {
+  //     if (param) {
+  //         setArticles(articles.reverse());
+  //     } else {
+  //         setArticles(_.sortBy(articles, tag));
+  //     }
+  //     setFunc(!param);
+  // }
+
+  return (
+    <div className="row mt-8">
+      <div className="col-md-15">
+
+       <div className="card">
+          <div className="card">
+            <div className="row">
+              <div className="card-title mr-5 ml-5 mt-2 mb-5">Shows</div>
+              <div className="card-title mr-5 ml-5 mt-2 mb-5">Name</div>
+              <div className="card-title mr-5 ml-5 mt-2 mb-5">Description</div>
+              <div className="card-title mr-5 ml-5 mt-2 mb-5" onClick={toggleFilterPrice}>Price</div>
+           </div>
+           
+                {shows.map((show, index) => {
+                  return (
+
+                    
+                    <tr className="py-2 tr-height" key={index}>
+                      <th className="card-text px-2" scope="row" width="50%">{show.name}</th>
+                      <td className="card-text" width="20%">{show.description}</td>
+                      
+                      <td className="card-text" width="20%">{show.price} $</td>
+          
+                      <td className="card-text" width="10%">{show.quantity}</td>
+                      <td width="20%">
+                        {/* <button onClick={() => props.dispatch(addArticle(article))} className="btn btn-primary btn-sm float-right mr-2">Add to cart</button> */}
+                      </td>
+                    </tr>
+                  )
+                })}
+
+          </div>
+
         </div>
-    )
+      </div>
+      <Cart />
+    </div>
+  )
 }
 
-export default connect()(ListArticles);
+export default Show;
 
 
 
